f448ea7e3d43b0bd47b32d6f715916e5a4c17ad6
CharlesV0 <101322423+CharlesV0@users.noreply.github.com>
2024-01-16 00:06:08 +0800

    Update momentum.m

--- project_momentum/momentum.m
+++ project_momentum/momentum.m
@@ -70,10 +70,7 @@ combinedDataset = resultTable;
 toc
 
 combinedDataset = rmmissing(combinedDataset);
-%% step 2: Portfolio Analysis
 combinedDataset.code = cell2table(combinedDataset.code);
-%combinedDataset.date = cell2table(combinedDataset.date);
-%combinedDataset.lme = cell2table(combinedDataset.lme);
 
 % finding out the problematic ones
 % store = zeros(1,500);
@@ -88,15 +85,10 @@ combinedDataset.code = cell2table(combinedDataset.code);
 % store = store(1,1:t);
 % combinedDataset(store,:) = [];
 
-% combinedDataset.K1 = table2array(cell2table(combinedDataset.k1));
-% combinedDataset.K3 = table2array(cell2table(combinedDataset.k3));
-% combinedDataset.K6 = table2array(cell2table(combinedDataset.k6));
-% combinedDataset.K12 = table2array(cell2table(combinedDataset.k12));
-% combinedDataset.K24 = table2array(cell2table(combinedDataset.k24));
 
-% combinedDataset = combinedDataset((combinedDataset.k1 > 0) ...
-% & (combinedDataset.k3 > 0)  & (combinedDataset.k6 > 0) ...
-% & (combinedDataset.k12 > 0) & (combinedDataset.k24 > 0), :);
+
+%% step 2: Portfolio Analysis
+
 
 [G,jdate]=findgroups(combinedDataset.date);
 
@@ -125,6 +117,7 @@ spread(i) = High_rr - Low_rr;
 end
 
 %% Q3
+timePoints = unique(combinedDataset.date);
 pca_table = zeros(length(timePoints),5);
 
 %填入k=3时group1-group5的平均收益
